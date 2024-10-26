a2a1f56d3915fdb71f3561a3bd12240192d46028
lidongjun2004 <litongjun20040927@outlook.com>
2024-10-26 19:20:09 +0800

    Site updated: 2024-10-26 19:20:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/hello-world/"];function toRandomPost(){
+var posts=["2024/08/31/hello-world/","2024/10/26/Markdown/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
