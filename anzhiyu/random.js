8d51463db1be6a62719ef2fe5f13ffdfe926ab96
lidongjun2004 <litongjun20040927@outlook.com>
2024-12-16 22:35:54 +0800

    Site updated: 2024-12-16 22:35:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/Markdown/","2024/08/31/hello-world/","2024/11/10/雅思/","2024/10/27/力学/"];function toRandomPost(){
+var posts=["2024/10/26/Markdown/","2024/08/31/hello-world/","2024/11/10/雅思/","2024/10/27/力学/","2024/12/16/光学/","2024/11/18/常用网站推荐/","2024/12/16/概率论/","2024/11/15/图论/","2024/12/16/数学建模/","2024/11/16/群论/","2024/12/16/热学/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
